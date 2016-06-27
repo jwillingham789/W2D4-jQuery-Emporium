ec025feccd743c755b17ceb82b101ddadebab9ea
Jason Lestina <coda19@gmail.com>
2016-06-27 13:54:06 -0400

    Tetris game now pauses when switching views, and resumes when returning to the tetris view. Added in a button to change the tetris theme randomly. Modified the CSS for the display box to fit the button. Added in some comments for code clarity

--- js/main.js
+++ js/main.js
@@ -7,35 +7,76 @@ var blockBtn = document.querySelector('.blockBtn'),
 
 
 blockBtn.addEventListener('click', function(){
+  //Show/hide the plugin views
   blockBox.classList.remove('hidden')
   fireBox.classList.add('hidden')
   planetBox.classList.add('hidden')
-  // OTHER WAY TO DO IT
 
-  // if (planetBox.style.display !== 'none') {
-  //   planetBox.classList.add('hidden')
-  // }
-  // else if (fireBox.style.display !== 'none') {
-  //   fireBox.classList.add('hidden')
-  // }
+  //Resume Tetris when switching back
+  $('.game').blockrain('resume');
 })
 
 fireBtn.addEventListener('click', function(){
+  //Show/hide the plugin views
   fireBox.classList.remove('hidden')
   blockBox.classList.add('hidden')
   planetBox.classList.add('hidden')
+
+  //Pause Tetris when switching to other views
+  $('.game').blockrain('pause');
 })
 
 planetBtn.addEventListener('click', function(){
+  //Show/hide the plugin views
   planetBox.classList.remove('hidden')
   fireBox.classList.add('hidden')
   blockBox.classList.add('hidden')
+
+  //Pause Tetris when switching to other views
+  $('.game').blockrain('pause');
 })
+
+//Randomize the tetris theme
+var themeButton = document.querySelector('.theme-button'),
+    themeArray = ['candy', 'modern', 'retro', 'vim', 'monochrome', 'gameboy', 'aerolab']
+
+themeButton.addEventListener("click", function() {
+  var randomNumber = Math.floor(Math.random() * 7)
+
+  if (themeArray[randomNumber] === 'candy') {
+    $('.game').blockrain('theme', 'candy')
+    themeButton.innerText = themeArray[randomNumber]
+  } else if (themeArray[randomNumber] === 'modern') {
+    $('.game').blockrain('theme', 'modern')
+    themeButton.innerText = themeArray[randomNumber]
+  } else if (themeArray[randomNumber] === 'retro') {
+    $('.game').blockrain('theme', 'retro')
+    themeButton.innerText = themeArray[randomNumber]
+  } else if (themeArray[randomNumber] === 'vim') {
+    $('.game').blockrain('theme', 'vim')
+    themeButton.innerText = themeArray[randomNumber]
+  } else if (themeArray[randomNumber] === 'monochrome') {
+    $('.game').blockrain('theme', 'monochrome')
+    themeButton.innerText = themeArray[randomNumber]
+  } else if (themeArray[randomNumber] === 'gameboy') {
+    $('.game').blockrain('theme', 'gameboy')
+    themeButton.innerText = themeArray[randomNumber]
+  } else if (themeArray[randomNumber] === 'aerolab') {
+    $('.game').blockrain('theme', 'aerolab')
+    themeButton.innerText = themeArray[randomNumber]
+  }
+})
+
 
 // jQuery Stuff
 $(document).ready(function() {
+    //Initialize the Tetris game
     $('.game').blockrain()
+
+    //Initialize the Burning text
     $('.burning').burn()
+
+    //Initialize the Planetarium
     $(".planet").planetarium({
        autospin: "5000ms",
        angle: "20deg",
